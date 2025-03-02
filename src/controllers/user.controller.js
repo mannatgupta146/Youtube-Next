eb5c39b00034aec822b73dccfd5d32b16517f65b
mannatgupta146 <mannatgupta146@gmail.com>
2025-03-03 01:33:39 +0530

    added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { ApiErrors } from "../utils/ApiErrors.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import mongoose from "mongoose";
 
 const generateAccessAndRefreshToken = async (userId) => {
     try {
@@ -336,8 +337,59 @@ const getChannelUserProfile = asyncHandler(async (req, res) => {
         throw new ApiErrors(404, "Channel doesn't exist");
     }
 
-    res.status(200).json(channel[0]); // Return the first matched channel
+    res.status(200).json(channel[0]); 
 });
+
+const getWatchHistory = asyncHandler(async (req, res) => {
+    const user= await User.aggregate([
+        {
+            $match:{
+                _id:new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup:{
+                from:"videos",
+                localField:"watchHistory",
+                foreignField:"_id",
+                as:"watchHistory",
+                pipeline:[
+                    {
+                    $lookup:{
+                        from:"users",
+                        localField:"owner",
+                        foreignField:"_id",
+                        as:"owner",
+                        pipeline:[
+                            {
+                                $project:{
+                                    fullName:1,
+                                    username:1,
+                                    avatar:1
+                                }
+                            },
+                            {
+                                $addFields:{
+                                    owner:{
+                                        $arrayElemAt:["$owner",0]
+                                    }
+                                }
+                            }
+                        ]
+
+                    }
+                }
+                  
+                ]
+            }
+        }
+        
+    ])
+
+    return res
+    .status
+    .json(new ApiResponse(200,user[0].getWatchHistory,"Watch history fetched successfully"))
+})
 
 export { 
     registerUser, 
@@ -349,6 +401,7 @@ export {
     updateAccountDetails, 
     updateUserAvatar,
     updateUserCoverImage,
-    getChannelUserProfile
+    getChannelUserProfile,
+    getWatchHistory
 
 };
